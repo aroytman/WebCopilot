ab8ceb8ace41da0af0b60cfd265aabd7ef0855bc
Alex Roytman <aroytman@profoundlogic.com>
2023-06-02 23:34:07 -0700

    Add testing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     './public/**/*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'xs': '0 0 15px 0 rgba(0, 0, 0, 0.1)',
+        // 'xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      },
+    },
   },
   plugins: [],
 }
